16ef2b235b211f029a145fc9aee621cf99c5c683
Damu1815 <153473405+Damu1815@users.noreply.github.com>
2023-12-14 17:07:29 +0530

    Add files via upload

--- hrapp/src/main/java/com/hr/repository/EmployeeRepository.java
+++ hrapp/src/main/java/com/hr/repository/EmployeeRepository.java
@@ -1,41 +1,44 @@
 package com.hr.repository;
+
 import java.time.LocalDate;
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.hr.entity.Employee;
+import com.hr.entity.Job;
 
 import jakarta.validation.Valid;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
- List<Employee> findByFirstName(String firstName);
 
- Employee findByEmail(@Valid String email);
+	List<Employee> findByFirstName(String firstName);
 
- Employee findByPhoneNumber(@Valid String phoneNumber);
+	Employee findByEmail(@Valid String email);
 
- List<Employee> findAllByCommissionPctIsNull();
+	Employee findByPhoneNumber(@Valid String phoneNumber);
 
- List<Employee> findAllByDepartmentDepartmentId(Long departmentId);
+	List<Employee> findAllByCommissionPctIsNull();
 
- List<Employee> findAllByHireDateBetween(LocalDate fromHireDate, LocalDate toHireDate);
- 
- @Query("SELECT e.department.departmentName,COUNT(e) from employee e GROUP BY e.deparment.departmentId")
- List<Object[]> countAllEmployeesGroupByDepartmentDepartmentId();
- @Query("SELECT j.jobTitle, MAX(e.salary) FROM Employee e JOIN e.job j WHERE e.employeeId = :employeeId GROUP BY j.jobTitle")
- List<Object[]> findMaxSalaryOfJobByemployeeId(@Param("employeeId")Long employeeId);
- 
- /*@Query("SELECT d.location.locationId, COUNT(e.employeeId) FROM Employee e JOIN e.department d GROUP BY d.location.locationId")
- List<Object[]> countAllEmployeesGroupByLocationId(); */
- 
- 
+	List<Employee> findAllByDepartmentDepartmentId(Long departmentId);
 
+	List<Employee> findAllByHireDateBetween(LocalDate fromHireDate, LocalDate toHireDate);
 
- 
- 
+	@Query("SELECT e.department.departmentName department, COUNT(e) count FROM Employee e GROUP BY e.department.departmentId")
+	List<Object[]> countAllEmployeesGroupByDepartmentDepartmentId();
+    
+	@Query("select e.department.location , count(e) from Employee e group by e.department.location")
+	List<Object[]> countAllEmployeesGroupByLocation();
   
+	/*@Query("select e.job from employee e where e.job.jobId NOT IN e.department")
+	List<Job> findAllOpenPositionsByDepartment(Long departmentId); */
+    
+	/*Long findMaxSalaryOfJobByEmployeeId(@Param("employeeId") Long employeeId);*/
+	
+	
+
 }
