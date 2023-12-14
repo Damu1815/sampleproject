148ef8d6313f012d83f0d055b6dbd13ce0675172
Bhanu2112 <bodasingibhanuprasad@gmail.com>
2023-12-14 17:06:45 +0530

    just chan

--- hrapp/src/main/java/com/hr/controller/EmployeeController.java
+++ hrapp/src/main/java/com/hr/controller/EmployeeController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hr.entity.Employee;
@@ -19,6 +22,22 @@ public class EmployeeController {
 	@GetMapping("/all")
 	public List<Employee> getAll() {
 		return employeeRepository.findAll();
+	}
+	
+	@GetMapping("/emp/{id}")
+	public Employee getByEId(@PathVariable Long id) {
+		return employeeRepository.findById(id).get();
+	}
+	
+	@PostMapping("/save")
+	public Employee saveEmp(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+	
+	@PostMapping("/save1")
+	public Employee getEmp(@RequestBody Employee employee) {
+		System.out.println(employee.getEmail());
+		return employee;
 	}
 
 }
--- hrapp/src/main/java/com/hr/entity/Country.java
+++ hrapp/src/main/java/com/hr/entity/Country.java
@@ -2,11 +2,16 @@ package com.hr.entity;
 
 
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -33,5 +38,6 @@ public class Country {
     @JoinColumn(name = "region_id")
     private Region region;
 
+  
   
 }
